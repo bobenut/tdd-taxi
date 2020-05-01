146ae594226b8d54dc2e7645178c76f92f0ab173
bobenut <kongxh_1981@aliyun.com>
2020-05-01 12:57:23 +0800

    view path

--- src/main/java/com/jiker/keju/AppRunner.java
+++ src/main/java/com/jiker/keju/AppRunner.java
@@ -5,12 +5,14 @@ import java.io.*;
 public class AppRunner {
 
     public static void main(String[] args) {
-        String testDataFile = args[0];
-        String userDir = System.getProperty("user.dir");
+
+//        String userDir = System.getProperty("user.dir");
+        String userDir = "src/main/resources";
         String inputPath = String.format("%s/%s", userDir, "input.txt");
         File file = new File(inputPath);
         System.out.println(String.format("arg0: %s => is %s", inputPath, file.exists()));
-        readfile(System.getProperty("user.dir"));
+
+        String testDataFile = args[0];
         String receipt = calculateFareFromFile(testDataFile);
         System.out.println(receipt);
     }
@@ -37,40 +39,4 @@ public class AppRunner {
         }
         return result;
     }
-
-    private static boolean readfile(String filepath) {
-
-        try {
-
-            File file = new File(filepath);
-            if (!file.isDirectory()) {
-                System.out.println("文件");
-                System.out.println("path=" + file.getPath());
-                System.out.println("absolutepath=" + file.getAbsolutePath());
-                System.out.println("name=" + file.getName());
-
-            } else if (file.isDirectory()) {
-                System.out.println("文件夹");
-                String[] filelist = file.list();
-                for (int i = 0; i < filelist.length; i++) {
-                    File readfile = new File(filepath + "\\" + filelist[i]);
-                    if (!readfile.isDirectory()) {
-                        System.out.println("path=" + readfile.getPath());
-                        System.out.println("absolutepath="
-                                + readfile.getAbsolutePath());
-                        System.out.println("name=" + readfile.getName());
-
-                    } else if (readfile.isDirectory()) {
-                        readfile(filepath + "\\" + filelist[i]);
-                    }
-                }
-
-            }
-
-        } catch (Exception e) {
-            System.out.println("readfile()   Exception:" + e.getMessage());
-        }
-        return true;
-    }
-
 }
